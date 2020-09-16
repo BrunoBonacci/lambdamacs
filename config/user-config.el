6b81131abc576e8c8733292dfc02816223adeddb
Bruno Bonacci <bruno.bonacci@gmail.com>
2020-09-16 18:39:18 +0100

    Update user-config.el

--- config/user-config.el
+++ config/user-config.el
@@ -6,13 +6,15 @@
 ;; (setq user-full-name    "Firstname Lastname"
 ;;       user-mail-address "your-email@email.org")
 
-
-
 ;; system-wide font size
-(setq lambdamacs/default-font-size 150)
 (setq lambdamacs/default-font "Roboto Mono Thin for Powerline") ;;"Fira Code Retina"
+(setq lambdamacs/default-font-size 150)
+
+;; themes to use
+;; suggestion: choose a dark and a light one, you will be switch between them
+;; using TODO:
 (setq lambdamacs/default-theme 'doom-moonlight)
-(setq lambdamacs/alternative-theme 'doom-moonlight)
+(setq lambdamacs/alternative-theme 'doom-nord-light)
 
 ;; directory where to save temp stuff
 (setq lambdamacs/save-place ".save")
