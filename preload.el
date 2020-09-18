68bbfb55c17fafaab5755077a5ca6b38d650efc4
Bruno Bonacci <bruno.bonacci@gmail.com>
2020-09-18 16:17:06 +0100

    removing unused file

--- preload.el
+++
@@ -1,6 +0,0 @@
-;; This file has configuration tweaks that are only meant to be used in Emacs
-;; From Scratch streams.
-
-;; Don't use my own package folder by accident
-(setq package-user-dir "~/work/oss/lambdamacs/packages")
-(setq user-emacs-directory "~/work/oss/lambdamacs")
