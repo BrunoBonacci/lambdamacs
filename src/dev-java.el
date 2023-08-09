8d7a629f314848f830565faa2c21474c6957a77e
Bruno Bonacci <bruno.bonacci@gmail.com>
2023-08-09 12:38:32 +0100

    fix lsp-java

--- src/dev-java.el
+++ src/dev-java.el
@@ -20,7 +20,4 @@
   (setq lsp-completion-enable-additional-text-edit nil))
 (use-package hydra)
 (use-package lsp-ui)
-(use-package lsp-java :config (add-hook 'java-mode-hook 'lsp))
-(use-package dap-mode :after lsp-mode :config (dap-auto-configure-mode))
-(use-package dap-java :ensure nil)
-(use-package lsp-treemacs)
+;;(use-package lsp-java :config (add-hook 'java-mode-hook 'lsp))
