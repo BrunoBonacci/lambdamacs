1597ceeea859cd1e96ac173760d8c7ea5c3a3657
Bruno Bonacci <bruno.bonacci@gmail.com>
2024-07-19 13:07:46 +0100

    update stylesheet cdn got orgmode

--- src/productivity.el
+++ src/productivity.el
@@ -100,7 +100,7 @@
 (setq org-html-head-include-scripts nil)
 (setq org-export-with-section-numbers nil)
 (setq org-html-head "
-<link rel=\"stylesheet\" type=\"text/css\" href=\"https://rawcdn.githack.com/BrunoBonacci/org-doc/master/assets/GTD.css\" />
+<link rel=\"stylesheet\" type=\"text/css\" href=\"https://cdn.jsdelivr.net/gh/BrunoBonacci/org-doc/assets/GTD.css\" />
 <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.2/styles/github.min.css\">
 
 <script src=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.7.2/highlight.min.js\"></script>
