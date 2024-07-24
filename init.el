a73bca330a80feef948b5e47557bc9df14dd0a18
Bruno Bonacci <bruno.bonacci@gmail.com>
2024-07-24 13:05:21 +0100

    small updates

--- init.el
+++ init.el
@@ -32,7 +32,7 @@
       '(("melpa-stable" . "https://stable.melpa.org/packages/")
         ("melpa" . "https://melpa.org/packages/")
         ("org"   . "https://orgmode.org/elpa/")
-        ;;("elpa"  . "https://elpa.gnu.org/packages/")
+        ("elpa"  . "https://elpa.gnu.org/packages/")
         ))
 
 (package-initialize)
