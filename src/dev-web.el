fc022e8923626c626f60a4a1d7dbfa129fc79ca7
Bruno Bonacci <bruno.bonacci@gmail.com>
2025-02-18 10:49:48 +0000

    edit to webdev

--- src/dev-web.el
+++ src/dev-web.el
@@ -1,3 +1,33 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                                                                            ;;
+;;                  ----==| H T M L   A N D   C S S |==----                   ;;
+;;                                                                            ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; consider skewer-mode
+;; https://github.com/skeeto/skewer-mode
+
+
+
+(use-package impatient-mode
+  :ensure t
+  :init
+  (setq httpd-port 8888)
+  (defun impatient-start (&optional arg)
+    "Attach a browser to Emacs for a skewer JavaScript REPL. Uses
+`browse-url' to launch a browser."
+    (interactive "p")
+    (httpd-start)
+    (impatient-mode)
+    (browse-url (format "http://127.0.0.1:%d/imp/" httpd-port)))
+  (defun impatient-stop (&optional arg)
+    "Attach a browser to Emacs for a skewer JavaScript REPL. Uses
+`browse-url' to launch a browser."
+    (interactive "p")
+    (httpd-stop)))
+
+
+
 (use-package emmet-mode
   :ensure t
   :init
